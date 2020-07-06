3f6af35eda4d638d21a377eb9c10c1c2f6684cf4
JGPico <minq_2@hotmail.com>
2020-07-06 16:53:34 -0600

    nine through eleven

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(num1, num2):
+    return num1 + num2
+
 
 print(f1(1, 2))
 
@@ -14,12 +17,25 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+
+def f2(*num):
+    sum = 0
+    for i in num:
+        if type(i) is list:
+            for j in i:
+                sum = sum + j
+        else:
+            sum = sum + i
+    return sum
+
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
+
 
 # How do you have to modify the f2 call below to make this work?
 print(f2(a))    # Should print 22
@@ -30,6 +46,11 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+
+def f3(num1=1, num2=1):
+    return num1 + num2
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +65,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**data):
+    for k, v in data.items():
+        print(f"key: {k}, value: {v}")
+
 
 # Should print
 # key: a, value: 12
@@ -61,5 +86,5 @@ d = {
     "hp": 3
 }
 
-# How do you have to modify the f4 call below to make this work?
-f4(d)
+# # How do you have to modify the f4 call below to make this work?
+f4(monster=d['monster'], hp=d['hp'])
